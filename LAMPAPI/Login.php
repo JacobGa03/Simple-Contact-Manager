1420ded531c3b5e7723bad7235cb5befda8cdd25
Pablo Rodriguez <pa233796@ucf.edu>
2024-05-20 17:20:46 -0400

    Login endpoint now verifies incoming JSON request and reports correct HTTP status codes

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,33 +1,28 @@
 <?php
-	//Used to login in an already existing user
-	$inData = getRequestInfo();
+
+  require 'common.php';
+
+  $required = ['username', 'password'];
+	$inData = getRequestParams($required);
 	
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "OurDatabase"); 	
-	if( $conn->connect_error )
+
+	$conn = getDbConnection(); 	
 	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		//This query works for selecting Users that are ALREADY in the database
 		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		//Bind the login and password to the '?' in our query
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->bind_param("ss", $inData["username"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
-		//Return the full name of the user along with their assigned ID
 		if( $row = $result->fetch_assoc()  )
 		{
 			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
 		}
-		//User isn't in the database
 		else
 		{
-			returnWithError("No Records Found");
+      returnError(CODE_NOT_FOUND, "No Records Found");
 		}
 
 		$stmt->close();
