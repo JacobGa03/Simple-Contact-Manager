e0dc267648a36ebe64b172bdafe1a10e15340f5f
Jacob Gadberry <jagadberry03@gmail.com>
2024-05-27 16:47:10 -0400

    Login/Register Connected and Work
    Login and Register pages work and are connected. I added custom error messages which let you know what and where you did something wrong.

--- js/code.js
+++ js/code.js
@@ -1,55 +1,108 @@
-
+//Grab the UN and PW from the input tags, then request the users info from the database
 async function doLogin() {
-  let requestData = {
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value
-  };
-
-  let [ code, result ] = await callApi("/Login.php", requestData);
-  switch(code)  {
-    case 200: // OK: Successful login
-      saveUser(result);
-      //TODO: Change this to whatever becomes the main dashboard page
-      window.location.replace('crud.html') 
-      break;
-    case 404: // NOT FOUND: Incorrect login
-      document.getElementById("loginResult").innerHTML = "Login/Password combination incorrect";
-      break;
-    default:
-      alert("An unknown error has occured. Check browser console for details.");
-      break;
-  }
-}
-
-async function doRegister() {
-  let password = document.getElementById("password").value;
-  let confirmPassword = document.getElementById("confirmPassword").value;
-
-  if(password != confirmPassword)
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  //Ensure that the fields are filled in for the API
+  if(username.value == '')
   {
-    document.getElementById("registerResult").innerHTML = "Make sure your passwords match";
+    username.reportValidity();
+    return;
+  }
+  if(password.value == '')
+  {
+    password.reportValidity();
     return;
   }
 
+  //Gather the data to be sent to the API
   let requestData = {
-    username: document.getElementById("username").value,
-    password: password,
-    firstName: document.getElementById("firstName").value,
-    lastName: document.getElementById("lastName").value
+    username: username.value,
+    password: password.value
+  };
+
+  let [ code, result ] = await callApi("/Login.php", requestData);
+  //Decode the response
+  switch(code)  {
+    case 200: // OK: Successful login
+      saveUser(result);
+      window.location.replace('dashboard.html');
+      break;
+    case 404: // NOT FOUND: Incorrect login
+      password.setCustomValidity("Login/Password Combination Incorrect");
+      password.reportValidity();
+      //Reset code so we can resubmit and run the form again (If we don't then we are stuck here)
+      code = 0;
+      break;
+    default:
+      alert("An unknown error has occurred. Check browser console for details.");
+      break;
+  }
+}
+//Create a new user
+async function doRegister() {
+  let firstName = document.getElementById("firstName");
+  let lastName = document.getElementById("lastName");
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
+  let confirmPassword = document.getElementById("confirmPassword");
+  //Do some sort of validation
+  if(firstName.value == '')
+  {
+    firstName.reportValidity();
+    return;
+  }
+  if(lastName.value == '')
+  {
+    lastName.reportValidity();
+    return;
+  }
+  if(username.value == '')
+  {
+    username.reportValidity();
+    return;
+  }
+  if(password.value == '')
+  {
+    password.reportValidity();
+    return;
+  }
+  if(confirmPassword.value == '')
+  {
+		confirmPassword.setCustomValidity("Confirm Your Password");
+    confirmPassword.reportValidity();
+    return;
+  }
+  //Ensure that both passwords are the same
+  if(password.value != confirmPassword.value)
+  {
+    //We can manipulate the "required" attribute to do some post-submission validation
+		confirmPassword.setCustomValidity("Passwords Must Match");
+    confirmPassword.reportValidity();
+    return;
+  }
+
+  //Gather the data to be sent to the API
+  let requestData = {
+    username: username.value,
+    password: password.value,
+    firstName: firstName.value,
+    lastName: lastName.value
   };
 
   let [ code, result ] = await callApi("/Register.php", requestData);
   switch(code) {
     case 200: // OK: Successful registration
       saveUser(result);
-      //TODO: Change this to whatever becomes the main dashboard page
-      window.location.replace('crud.html') 
+      window.location.replace('dashboard.html');
       break;
     case 409: // CONFLICT: Requested username already exists
-      document.getElementById("registerResult").innerHTML = "Username already taken. Please choose another one.";
+		  username.setCustomValidity("Username Already Taken. Please Choose Another One.");
+      username.reportValidity();
+      //Reset code so we can resubmit and run the form again (If we don't then we are stuck here)
+      code = 0;
       break;
     default:
-      alert("An unknown error has occured. Check browser console for details.");
+      alert("An unknown error has occurred. Check browser console for details.");
       break;
   }
 }
