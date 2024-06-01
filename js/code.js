7726ee94fcca4395b6c4e49e0de2afb7465f2454
zachcary1 <61954066+zachcary1@users.noreply.github.com>
2024-05-31 21:54:19 -0400

    added logout button
    logout button with functionality, deletes cookie that has all user info stored, and reroutes to login page

--- js/code.js
+++ js/code.js
@@ -214,3 +214,118 @@ function getUser() {
   }
   return obj;
 }
+<<<<<<< Updated upstream
+=======
+/* Draw a new <tr> for all of the contacts within result (array)
+*  The contact info and buttons are also drawn.
+*/
+function drawRow(result){
+  // retrieve the HTML table element
+  let table = document.getElementById("contacts-table-tbody");
+
+  // the functions that will be attached to ALL the Edit/Delete buttons
+  let editCallback = (evt) => {
+    // get the element that got triggered (ie, the button)
+    let editButton = evt.currentTarget;
+    // show the HTML overlay
+    let overlay = document.getElementById("eoverlay");
+    overlay.classList.add("active");
+
+    // fill the textboxes of the overlay with existing contact data
+    document.getElementById("edit-firstName").value = editButton.associatedContact.firstName;
+    document.getElementById("edit-lastName").value = editButton.associatedContact.lastName;
+    document.getElementById("edit-email").value = editButton.associatedContact.email;
+    document.getElementById("edit-phone").value = editButton.associatedContact.phone;
+
+    // the confirm button of the overlay...
+    let confirmButton = document.getElementById("confirm-edit");
+    // ...attach it to the edit function and bind contact id
+    confirmButton.addEventListener("click", () => {
+      updateContact(editButton.associatedContact.id);
+      overlay.classList.remove("active"); // hide overlay
+      // get the row of the table...
+      let tableRow = editButton.closest("tr");
+      // and changes the name, phone and mail to match update
+      let newfirstname = document.getElementById("edit-firstName").value;
+      let newlastname = document.getElementById("edit-lastName").value;
+      let newphone = document.getElementById("edit-phone").value;
+      let newemail = document.getElementById("edit-email").value;
+      editButton.associatedContact.firstName = newfirstname;
+      editButton.associatedContact.lastName = newlastname;
+      editButton.associatedContact.phone = newphone;
+      editButton.associatedContact.email = newemail;
+      tableRow.querySelector("#nameCell").replaceChildren(document.createTextNode(newfirstname + ' ' + newlastname));
+      tableRow.querySelector("#phoneCell").replaceChildren(document.createTextNode(newphone));
+      tableRow.querySelector("#emailCell").replaceChildren(document.createTextNode(newemail));
+     })
+  };
+
+  let deleteCallback = (evt) => {
+    // get the element that got triggered (ie, the button)
+    let deleteButton = evt.currentTarget;
+
+    // show the HTML overlay
+    let overlay = document.getElementById("doverlay");
+    overlay.classList.add("active");
+    // the confirm button of the overlay...
+    let confirmButton = document.getElementById("confirm-delete");
+    // ...attach it to the delete function and bind contact id
+    confirmButton.addEventListener("click", () => {
+      deleteContact(deleteButton.associatedContact.id);
+      overlay.classList.remove("active"); // hide overlay
+      // get the row of the table...
+      let tableRow = deleteButton.closest("tr");
+      table.removeChild(tableRow); // ...and delete it
+    });
+  };
+
+  // iterate over the received array of objects
+  for(contact of result["Results"]) {
+    // create entries on the HTML table for each contact
+    //TODO: Can we someway add some properties so we can make delete easier?
+    let newRow = table.insertRow();
+    // Set the id of the new row to ('row' + contact.id)
+    newRow.setAttribute("id", "row"+contact.id);
+    let nameCell = newRow.insertCell();
+    let emailCell = newRow.insertCell();
+    let phoneCell = newRow.insertCell();
+    nameCell.appendChild(document.createTextNode(contact.firstName + " " + contact.lastName));
+    nameCell.setAttribute("id", "nameCell");
+    emailCell.appendChild(document.createTextNode(contact.email));
+    emailCell.setAttribute("id", "emailCell");
+    phoneCell.appendChild(document.createTextNode(contact.phone));
+    phoneCell.setAttribute("id", "phoneCell");
+    // create the two buttons and add the necessary class plus text
+    let buttonsCell = newRow.insertCell();
+    let buttonEdit = document.createElement("button");
+    let buttonDelete = document.createElement("button");
+    buttonEdit.setAttribute("class", "buttonY");
+    buttonDelete.setAttribute("class", "buttonR");
+    buttonEdit.appendChild(document.createTextNode("Edit"));
+    buttonDelete.appendChild(document.createTextNode("Delete"));
+    // this is the trick - assign a custom parameter to the element itself!
+    // go ahead and just copy the whole contact to the button's internal object
+    buttonEdit.associatedContact = contact;
+    buttonDelete.associatedContact = contact;
+    // when clicked, the button will call the function and
+    // pass itself as an argument
+    buttonEdit.addEventListener("click", editCallback);
+    buttonDelete.addEventListener("click", deleteCallback);
+    // place them in the table
+    buttonsCell.appendChild(buttonEdit);
+    buttonsCell.appendChild(buttonDelete);
+  }
+}
+
+
+// Logout function
+async function doLogout() {
+  // Clear user cookie
+  document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  // Redirect to login page
+  window.location.replace('login.html');
+}
+
+// Add event listener for logout confirmation button
+document.getElementById("confirm-logout").addEventListener("click", doLogout);
+>>>>>>> Stashed changes
